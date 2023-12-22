4855eaa17af46602ed6f65d4e769697a5a157094
Sekqies <fernando.ellerbrock@gmail.com>
2023-12-22 18:08:11 -0300

    Melhorada compreensão de funções

--- DomainColoring.js
+++ DomainColoring.js
@@ -1,4 +1,4 @@
-let canvas, imageData, data, width, height;
+let canvas, imageData, data, width, height; 
 var qtdInteiros = 10;
 var centro = 300;
 function HSLtoRGB(h, s, l) {
@@ -40,6 +40,7 @@ function getNumeroInteiro(){
 }
 
 function Eixos (imgDataEixos, numeroInteiro){
+    if (false) {
     let dataEixos = imgDataEixos.data;
     
     let posicoesInteiros = [];
@@ -84,6 +85,7 @@ function Eixos (imgDataEixos, numeroInteiro){
                 }
             });
         }
+    }
     }
 
 
@@ -132,7 +134,6 @@ function Complexo_1(canvasElement, tipo_grafico,funcao,resultado_real,resultado_
             
             let real = eval(resultado_real);
             let imag = eval(resultado_imag);
-
             //f(z) -> z^3 - 1
             //let real = a*a*a - 3*a*b*b - 1;
             //let imag = 3*a*a*b - b*b*b;
@@ -187,26 +188,53 @@ function Complexo_1(canvasElement, tipo_grafico,funcao,resultado_real,resultado_
 }
 
 function simplificarExponencial(inputFunction) {
-    //Transforma uma função em formato de a^b em a*a*a...*a (b vezes)
-    
-    //divide a função em base e expoente onde houver o '^'
+    //Transforma uma função em formato de a^b em Math.pow(a,b)
 
     var outputFunction = inputFunction.replace(/(\w+)\s*\^\s*(\w+)/g, function(match, base, exponent) {
         return 'Math.pow(' + base + ', ' + exponent + ')';
     });
 
     return outputFunction;
-    
-    /*
-    var funcaoOutput = inputFunction.replace(/(\w+)\s*\^\s*(\w+)/g, function(match, base, exponent) {
-      
-      var retornolambda = Array.from({ length: parseInt(exponent, 10) }, () => base).join('*');
-      return retornolambda;
-      
-    });
-  
-    return funcaoOutput;*/
   }
+
+  function simplificarExpoenteImaginario(funcaoOriginal) {
+    // Um expoente no formato de i^1 = i^5 = i^9. Ou seja, i^x, onde x é um número inteiro, é sempre igual a i^x%4
+    // Além disso, o Algebrite não sabe fazer operações com i elevado a potências maiores que 5.
+
+    const regex = /(\bj\b)\^(\w+)/g;
+    const funcaoSimplificada = funcaoOriginal.replace(regex, function(match, base, expoente) {
+      // Replace b with the value of the expression b%4
+      return base + "^(" + eval(expoente + " % 4") + ")";
+    });
+    return funcaoSimplificada;
+  }
+
+
+function identificarExpoenteComplexo(inputString) {
+    var outputString = inputString.replace(/(\w+)\^z/g, function(match, base) {
+        return `${base}^a * (cos(b*ln(${base})) + i*sin(b*ln(${base})))`
+    });
+    return outputString;
+}
+
+function limparFuncao(funcaoOriginal) {
+    var funcaoLimpa = funcaoOriginal.replace(/sin\((.*?)\)/g, 'Math.sin($1)');
+    funcaoLimpa = funcaoLimpa.replace(/cos\((.*?)\)/g, 'Math.cos($1)');
+    funcaoLimpa = funcaoLimpa.replace(/log\((.*?)\)/g, 'Math.log($1)');
+    funcaoLimpa = funcaoLimpa.replace(/abs\((.*?)\)/g, 'Math.abs($1)');
+    funcaoLimpa = funcaoLimpa.replace(/tan\((.*?)\)/g, 'Math.tan($1)');
+    funcaoLimpa = funcaoLimpa.replace(/asin\((.*?)\)/g, 'Math.asin($1)');
+    funcaoLimpa = funcaoLimpa.replace(/acos\((.*?)\)/g, 'Math.acos($1)');
+    funcaoLimpa = funcaoLimpa.replace(/atan\((.*?)\)/g, 'Math.atan($1)');
+    funcaoLimpa = funcaoLimpa.replace(/sqrt\((.*?)\)/g, 'Math.sqrt($1)');
+    funcaoLimpa = funcaoLimpa.replace(/exp\((.*?)\)/g, 'Math.exp($1)');
+    funcaoLimpa = funcaoLimpa.replace(/arg\((.*?)\)/g, 'Math.arg($1)');
+    funcaoLimpa = funcaoLimpa.replace(/pi/g, 'Math.PI');
+    funcaoLimpa = funcaoLimpa.replace(/e/g, 'Math.E');
+    return funcaoLimpa;
+}
+
+
 
 function init(){
     let canvasElement = document.getElementById("domainColorCanvas");
@@ -228,7 +256,7 @@ function init(){
 
     //Função:
     let funcao = document.getElementById('funcao_complexa').value;
-    let operacao = "z = a + b*i \n" + funcao;
+    let operacao = "z = a + b*j \n" + identificarExpoenteComplexo(funcao);
     //alert(funcao);
 
     //se houver uma letra que não seja Z (ou i), dá um alert de erro
@@ -254,14 +282,18 @@ function init(){
     
     else {
     let resultado = Algebrite.run(operacao);
+    resultado= simplificarExpoenteImaginario(resultado);
+    resultado = Algebrite.subst("i","j",resultado);
+    resultado = Algebrite.run(resultado.toString());
     console.log("Função final: " + resultado);
-
-    let resultado_real = Algebrite.real(resultado).toString();
-    let resultado_imag = Algebrite.imag(resultado).toString();
+    let resultado_real = Algebrite.simplify(Algebrite.real(resultado).toString()).toString();
+    let resultado_imag = Algebrite.simplify(Algebrite.imag(resultado).toString()).toString();
     console.log("Parte real antes do processamento: " + resultado_real);
     console.log("Parte imaginária antes do processamento: " + resultado_imag);
     //Substitui ^ para vários * para que o eval possa entender
     resultado_real = simplificarExponencial(resultado_real);
+    resultado_real = limparFuncao(resultado_real);
+    resultado_imag = limparFuncao(resultado_imag);
     resultado_imag = simplificarExponencial(resultado_imag);
     console.log("Parte real após o processamento: " + resultado_real);
     console.log("Parte imaginária após o processamento: " + resultado_imag);
