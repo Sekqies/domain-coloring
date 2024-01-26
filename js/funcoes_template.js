34094ddabc70cf263ffacf9bafbbd88a41465e16
Sekqies <fernando.ellerbrock@gmail.com>
2024-01-26 12:22:10 -0300

    Programa otimizado consideravelmente

--- js/funcoes_template.js
+++ js/funcoes_template.js
@@ -1,1 +1,1 @@
-var funcaoBase = '<m v="1.2.0"><e></e><f type="function" group="functions"><b p="latex">z</b><b p="small_latex">z</b><b p="asciimath">z</b></f><e>+2</e></m>';
+var funcaoBase = '<m v="1.2.0"><e></e><f type="z" group="z"><b p="latex">z</b><b p="small_latex">z</b><b p="asciimath">z</b></f><e>+2</e></m>';
