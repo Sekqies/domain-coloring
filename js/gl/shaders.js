f158a6e5048cb597313b61cbb6bf2a4567edcf67
Sekqies <fernando.ellerbrock@gmail.com>
2024-03-11 16:47:41 -0300

    Resolvido problema de gráfico com webGL ficando preto

--- js/gl/shaders.js
+++ js/gl/shaders.js
@@ -1,5 +1,5 @@
 function writeFragmentShader(funcao, width, height, inteiros) {
-    console.log(width)
+    console.log(width,height)
     let continuo = tipo_grafico == 1;
     let vazio = funcao === '';
     return `
--- js/setup/hover_setup.js
+++ js/setup/hover_setup.js
@@ -6,7 +6,6 @@ function loadHover(funcaoHover, nomeStringCanvas) {
     nomeCanvas.addEventListener('mousemove', function (event) {
         const x = event.offsetX;
         const y = event.offsetY;
-        console.log(x, y);
 
         if (nomeStringCanvas == "glCanvas") {
             descricaoFuncao.style.top = y + 'px';
